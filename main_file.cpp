a70f83a58e7eea025f08a433598a83693d016dc9
Jedrzej Smok <jedrzejsmok55@gmail.com>
2021-05-14 22:05:09 +0200

    556

--- main_file.cpp
+++ main_file.cpp
@@ -60,7 +60,7 @@ GLuint readTexture(const char* filename) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	return tex;
+	return tex;//ffd
 }
 
 //Odkomentuj, ¿eby rysowaæ kostkê
