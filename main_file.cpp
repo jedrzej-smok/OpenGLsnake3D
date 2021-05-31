afc9e60c6df3a21d41bc58e492bd24e271c9f23d
jedrzej <jedrzejsmok55@gmail.com>
2021-05-31 17:56:34 +0200

    jablko,ale przerywa

--- main_file.cpp
+++ main_file.cpp
@@ -53,11 +53,17 @@ glm::mat4 Pglobal = glm::perspective(50.0f * PI / 180.0f, aspectRatio, 0.01f, 50
 glm::mat4 Mglobal = glm::mat4(1.0f);
 bool collisionWithApple = false;
 float distCollisionHead = 3.0f;
+float distCollisionApple = 3.0f;
+float distRandApple = 2.0f;
+bool existingApple = false;
+int weight = 800;
+int height = 800;
 
 //modele============================================================
 myModel3D head;
 myModel3D ball;
 myModel3D tail;
+myModel3D apple;
 std::vector<std::vector<float>> tailCoords;
 std::vector<std::vector<float>> flexions;
 
@@ -111,6 +117,12 @@ void initOpenGLProgram(GLFWwindow* window) {
 	tail.setupModel(0, 0, 0, 0, 0, 0, 1, 1, 1);
 	tail.initModel("modele/ball.obj", "modele/zielony.png", "modele/sky.png");
 
+	//apple.initModel("modele/apple2.obj", "modele/apple2.png", "modele/sky.png");
+	//apple.setupModel(0, 0, 0, PI/2, 0, 0, 30.f, 30.f, 30.f);
+
+	apple.initModel("modele/apple3.obj", "modele/apple3.png", "modele/sky.png");
+	apple.setupModel(-5, 0, 0, PI / 2, 0, 0, 2.f, 2.f, 2.f);
+
 }
 
 
@@ -121,6 +133,7 @@ void freeOpenGLProgram(GLFWwindow* window) {
 	ball.freeModel();
 	head.freeModel();
 	tail.freeModel();
+	apple.freeModel();
 	//===========
 	delete sp;
 }
@@ -137,12 +150,17 @@ void drawScene(GLFWwindow* window) {
 	ball.drawModel(Vglobal, Pglobal, Mglobal, (sin(ball.angle_x))*speed_y,0, (cos(ball.angle_x))*speed_y, speed_x*frameTime, 0, 0, 1, 1, 1);
 	head.drawModel(Vglobal, Pglobal, ball.matrixM, 0,0,0,0, 0, 0, 1,1,1);
 	
+	if (1/*existingApple*/) {
+		apple.drawModel(Vglobal, Pglobal, Mglobal, 0, 0, 0, speed_x * frameTime, 0, 0, 1, 1, 1);
+		std::cout << apple.world_coord_x << ", " << apple.world_coord_z << "\n";
+	}
 	
 	
 	//zapisz miejsce ball
 	flexions.insert(flexions.begin(), std::vector<float>{ball.coord_x, ball.coord_y,ball.coord_z, ball.angle_x});
 	//std::cout << "coord_x:" << ball.coord_x << ", coord_z:" << ball.coord_z << "\n";
 	//std::cout <<"tail size:"<< tailCoords.size()<<"\n";
+	
 	for (int t = 0; t < tailCoords.size(); t++) {
 		if (flexions.size() >= t) {
 			//std::cout <<"t: "<<t<<"; "<< flexions[t][0] << "," << flexions[t][1] << "," << flexions[t][2] << "," << flexions[t][3] << "\n";
@@ -154,12 +172,8 @@ void drawScene(GLFWwindow* window) {
 		}
 	}
 	
-	//std::cout << "balls[0].angle_x: " << balls[0].angle_x << "\n";
-	//std::cout << "-sin(balls[0].angle_x)*ballSize:" << -sin(balls[0].angle_x) * ballSize << "\n";
-	//std::cout << "-cos(balls[0].angle_x)*ballSize:" << -cos(balls[0].angle_x) * ballSize << "\n\n\n";
-
-
-
+	
+	
 //reszta=====================================================================
     glfwSwapBuffers(window); //Przerzuæ tylny bufor na przedni
 }
@@ -180,12 +194,69 @@ void detectCollisionItself() {
 			(tailCoords[t][2] - head.world_coord_z) * (tailCoords[t][2] - head.world_coord_z));
 		//zderzy sie ze soba
 		if (dist < distCollisionHead) {
-			std::cout << "zderzenie";
+			std::cout << "zderzenie ze soba";
+			std::cout << "\nt:" << t << std::endl;
 			Sleep(3000);
 			exit(0);
 		}
 	}
 }
+
+void randApple() {
+	if (collisionWithApple == true) {
+		//zostalo zjedzone
+		int x, z;
+		float dist;
+		bool out = false;
+		do {
+			out = false;
+			x = rand() % 20 - 10;
+			z = rand() % 10 - 5;
+
+			dist = sqrt((head.world_coord_x - x) * (head.world_coord_x - x) +
+				(head.world_coord_z - z) * (head.world_coord_z - z));
+			//zderzy sie ze soba
+			if (dist < distRandApple) {
+				out = true;
+			}
+			dist = sqrt((ball.world_coord_x - x) * (ball.world_coord_x - x) +
+				(ball.world_coord_z - z) * (ball.world_coord_z - z));
+			//zderzy sie ze soba
+			if (dist < distRandApple) {
+				out = true;
+			}
+
+			for (int t = 0; t < tailCoords.size(); t++) {
+				dist = sqrt((tailCoords[t][0] - x) * (tailCoords[t][0] - x) +
+					(tailCoords[t][2] - z) * (tailCoords[t][2] - z));
+				//zderzy sie ze soba
+				if (dist < distRandApple) {
+					break;
+				}
+			}
+		} while (dist < distRandApple || out);//zeby nie bylo na wezu
+
+		apple.setupModel((float)x, 0, (float)z, 0, 0, 0, 2.f, 2.f, 2.f);
+
+
+	}
+}
+
+void detectCollisionApple() {
+	float dist;
+	dist = sqrt((apple.world_coord_x - head.world_coord_x) * (apple.world_coord_x - head.world_coord_x) +
+			(apple.world_coord_y - head.world_coord_y) * (apple.world_coord_y - head.world_coord_y) +
+			(apple.world_coord_z - head.world_coord_z) * (apple.world_coord_z - head.world_coord_z));
+		//zderzy sie ze soba
+		if (dist < distCollisionApple) {
+			std::cout << "\n najechane jablko\n";
+			
+			collisionWithApple = true;//powieksz weza
+			
+		}
+		randApple();
+}
+
 
 int main(void)
 {
@@ -198,7 +269,7 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);  //Utwórz okno 500x500 o tytule "OpenGL" i kontekst OpenGL.
+	window = glfwCreateWindow(weight, height, "OpenGL", NULL, NULL);  //Utwórz okno 500x500 o tytule "OpenGL" i kontekst OpenGL.
 
 	if (!window) //Je¿eli okna nie uda³o siê utworzyæ, to zamknij program
 	{
@@ -226,11 +297,18 @@ int main(void)
 		drawScene(window); //Wykonaj procedurê rysuj¹c¹
 		glfwPollEvents(); //Wykonaj procedury callback w zaleznoœci od zdarzeñ jakie zasz³y.
 		
+		
 		//=======================================
 		//najpierw detect apple potem
+		
+		
+		detectCollisionApple();
 		addball();
+		
 		//czy zderzy sie ze soba
+		
 		detectCollisionItself();
+		
 		Sleep(3000.f / 60.f);
 	}
 
